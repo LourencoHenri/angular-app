73a445f865bbe140cff233f738d5f4746be42453
Lourenço <henri.lourenco@outlook.com>
2024-09-23 22:40:31 -0300

    [Add] Update - Home Screen

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  logoUrl = '../assets/logo.svg';
 }
